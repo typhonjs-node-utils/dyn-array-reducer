e29c9b18f3459535178f4990ca37b3056ef72135
typhonrt <support@typhonjs.io>
2022-01-21 16:55:40 -0800

    running a build test

--- test/src/node/run.test.js
+++ test/src/node/run.test.js
@@ -4,10 +4,13 @@ import * as Module         from '../../../src/DynArrayReducer.js';
 
 import TestsuiteRunner     from '../runner/TestsuiteRunner.js';
 
+console.log('! - run.test.js - debug - 0');
 fs.ensureDirSync('./.nyc_output');
+console.log('! - run.test.js - debug - 1');
 fs.emptyDirSync('./.nyc_output');
 
 fs.ensureDirSync('./coverage');
 fs.emptyDirSync('./coverage');
 
+console.log('! - run.test.js - debug - 2');
 TestsuiteRunner.run({ Module });
